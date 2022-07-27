53ef859c081db7e9b2a867674052c28016124395
Tushar <tushargarg895@gmail.com>
2022-07-27 17:07:56 +0530

    initial

--- config/db.js
+++ config/db.js
@@ -1,14 +1,19 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 function connectDB() {
-    // Database connection 🥳
-    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
-    const connection = mongoose.connection;
-    connection.once('open', () => {
-        console.log('Database connected 🥳🥳🥳🥳');
-    }).catch(err => {
-        console.log('Connection failed ☹️☹️☹️☹️');
-    });
+  // Database connection 🥳
+  mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  });
+  mongoose.connection.on("error", (err) => {
+    console.log("Error connecting mongoDB", err);
+  });
+  mongoose.connection.on("connected", () => {
+    console.log("Conneted to mongoDB");
+  });
 }
 
 // mIAY0a6u1ByJsWWZ
